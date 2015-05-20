fdec4175e3842bca35c9d5d6c1857804984aab47
sbeamish <scott.beamish@gmail.com>
2015-05-20 13:31:48 -0500

    Commit

--- scripts/Start-DockerDemo.ps1
+++ scripts/Start-DockerDemo.ps1
@@ -1,37 +1,24 @@
 ﻿$Env:Path = "${Env:Path};c:\Program Files (x86)\Git\bin"
 
 $Env:DOCKER_HOST = "tcp://192.168.59.103:2376"
+$Env:DOCKER_CERT_PATH = "$env:USERPROFILE\.boot2docker\certs\boot2docker-vm"
 $Env:DOCKER_TLS_VERIFY = "1"
 
 boot2docker start
 
-# run interactively
-#docker run -t -i ubuntu:14.04 echo 'Hello World'
-#docker run -t -i ubuntu:14.04 /bin/bash
-
-# run as a daemon
-#docker run -d -P --name ra training/webapp python app.py
-
-#boot2docker ip
-
-#docker ps
-
-#docker port silly_payne 5000
-
-#docker logs -f silly_payne
-
-#docker top silly_payne
-
-#docker inspect silly_payne
-
 # Setup Docker Demo commands
-#docker pull mysql
-#docker pull devtest latest
-#docker pull rademo latest
+#docker pull mysql:tag
+#docker pull devtest:tag
+#docker pull rademo:tag
 
+#remove existing names
+Write-Host
+Write-host "Removing any pre-existing container tags"
+docker rm -f ra
+docker rm -f mysql
 
 # Start building commands
-#docker run -d --name ra --link mysql:mysql ubuntu:14.04 /bin/bash
-#docker run -d mysql /bin/bash
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=cademo -d mysql
+docker run -it --name ra  --link mysql:mysql -v //E:/Source:/opt/share ubuntu:14.04
 
-docker run -d --name ra ubuntu:14.04
+####
