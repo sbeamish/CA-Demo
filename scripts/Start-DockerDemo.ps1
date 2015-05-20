cb8c8de425a085677061591c866c83c10304b42f
sbeamish <scott.beamish@gmail.com>
2015-05-20 12:33:32 -0500

    changed user specic values

--- scripts/Start-DockerDemo.ps1
+++ scripts/Start-DockerDemo.ps1
@@ -1,7 +1,6 @@
 ﻿$Env:Path = "${Env:Path};c:\Program Files (x86)\Git\bin"
 
 $Env:DOCKER_HOST = "tcp://192.168.59.103:2376"
-$Env:DOCKER_CERT_PATH = "C:\Users\scott\.boot2docker\certs\boot2docker-vm"
 $Env:DOCKER_TLS_VERIFY = "1"
 
 boot2docker start
@@ -11,7 +10,7 @@ boot2docker start
 #docker run -t -i ubuntu:14.04 /bin/bash
 
 # run as a daemon
-#docker run -d -P training/webapp python app.py
+#docker run -d -P --name ra training/webapp python app.py
 
 #boot2docker ip
 
@@ -25,11 +24,14 @@ boot2docker start
 
 #docker inspect silly_payne
 
-
 # Setup Docker Demo commands
 #docker pull mysql
 #docker pull devtest latest
 #docker pull rademo latest
 
-#docker run -d ubuntu:14.04 /bin/bash
+
+# Start building commands
+#docker run -d --name ra --link mysql:mysql ubuntu:14.04 /bin/bash
 #docker run -d mysql /bin/bash
+
+docker run -d --name ra ubuntu:14.04
