61ad161070cfbbd86741d2d859d9690c591d6bcc
sbeamish <scott.beamish@gmail.com>
2015-05-20 10:51:10 -0500

    Added Start-Docker PowerShell Script

---
+++ Start-Docker.ps1
@@ -0,0 +1,23 @@
+﻿$Env:Path = "${Env:Path};c:\Program Files (x86)\Git\bin"
+
+$Env:DOCKER_HOST = "tcp://192.168.59.103:2376"
+$Env:DOCKER_CERT_PATH = "C:\Users\scott\.boot2docker\certs\boot2docker-vm"
+$Env:DOCKER_TLS_VERIFY = "1"
+
+boot2docker start
+
+#docker run -t -i ubuntu:14.04 echo 'Hello World'
+
+#docker run -d -P training/webapp python app.py
+
+#boot2docker ip
+
+#docker ps
+
+#docker port silly_payne 5000
+
+#docker logs -f silly_payne
+
+#docker top silly_payne
+
+#docker inspect silly_payne
