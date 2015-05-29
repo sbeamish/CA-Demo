412e803ca24356d10b869b883463cca36ac79d48
sbeamish <scott.beamish@gmail.com>
2015-05-29 11:02:36 -0500

    Incremental Update

--- scripts/Start-DockerDemo.ps1
+++ scripts/Start-DockerDemo.ps1
@@ -6,21 +6,22 @@ $Env:DOCKER_TLS_VERIFY = "1"
 
 boot2docker start
 
-# Setup Docker Demo commands
-#docker pull mysql:tag
-#docker pull devtest:tag
-#docker pull rademo:tag
-
 #remove existing names
 Write-Host
 Write-host "Stop/remove any pre-existing container tags"
 
-docker stop ra
-docker stop mysql
-
 docker rm -f ra
 docker rm -f mysql
+docker rm -f devtest
+docker rm -f cms
 
-# Start building commands
-docker run --name mysql -e MYSQL_ROOT_PASSWORD=cademo -d mysql 
-docker run -itd --name ra --link mysql:mysql -p 8080:8080 -v /c/Users:/mnt ubuntu:14.04 bin/bash
+#remove existing names
+Write-Host
+Write-host "Start containers"
+
+docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=cademo mysql 
+docker run -itd --name cms -p 8090:8080 -p 9990:9990 caiw/cms
+docker run -itd --name ra --link mysql:mysql --link cms:cms -p 8080:8080 -v /c/Users:/mnt ubuntu:14.04 bin/bash
+
+docker run -itd --name devtest -p 15006:1506 -p 15005:1505 -p 15007:1507 -p 20008:2008 -p 20009:2009 -p 20100:2010 -p 20130:2013 -p 20140:2014 -p 10980:1098 -p 10990:1099 -p 8081:8081 -p 8082:8082 -p 9090:9090 caiw/devtest:8.0.2 /bin/bash
+docker exec -it devtest /usr/local/CA/DevTest/bin/entrypoint.sh
